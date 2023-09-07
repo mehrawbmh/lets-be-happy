afd327cc973ef11065a88f62def6538cf45cdcac
Mehrab <mehrabmh81@yahoo.com>
2023-09-07 19:01:47 +0330

    add basic settings file with mongo configs, also connect it to .env file

--- configs/settings.py
+++ configs/settings.py
@@ -1,4 +1,27 @@
 # TODO: make it singleton
-class Settings:
+from pathlib import Path
+
+from pydantic.v1 import BaseSettings
+
+
+class Settings(BaseSettings):
+    class Config:
+        """
+        Config .env file for project credential and environment stuff
+        """
+        env_file = '.env'
+        env_file_encoding = 'utf-8'
+        fields_list = ['SECRET_KEY', 'MONGO_HOST', 'MONGO_USERNAME', 'MONGO_PORT', 'MONGO_PASSWORD', 'MONGO_DB']
+        fields = {item: {'env': item} for item in fields_list}
+
     MONGO_USERNAME = ""
     MONGO_PASSWORD = ""
+    MONGO_DB = "main"
+    MONGO_PORT = "27017"
+    MONGO_HOST = "localhost"
+
+    BASE_DIR = str(Path(__file__).resolve().parent.parent)
+    PROJECT_NAME = str("lets-be-happy")
+
+
+settings = Settings()
