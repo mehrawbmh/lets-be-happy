1cf0190a253c2da50b038765ebb163caffe92319
Mehrab <mehrabmh81@yahoo.com>
2023-09-08 11:19:07 +0330

    better directories for now

---
+++ core/auth/authentication.py
@@ -0,0 +1,15 @@
+from fastapi import Depends
+
+from models.users import User
+from passlib.context import CryptContext
+from dependencies.depends import get_main_db
+
+
+# TODO: sanitize, escape or remove and error while getting injection with non-alphanumeric characters
+class Authentication:
+    @staticmethod
+    def __check_password(user: User, given_password: str):
+        return user.password == given_password
+
+    async def find_user_by_username(self, username: str, db=Depends(get_main_db)) -> User:
+        ...
