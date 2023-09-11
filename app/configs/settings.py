98f9fc39a948e3b05b5feac3e7d22e346894df4d
Mehrab <mehrabmh81@yahoo.com>
2023-09-11 10:48:28 +0330

    add comment to check later

--- app/configs/settings.py
+++ app/configs/settings.py
@@ -9,7 +9,7 @@ class Settings(BaseSettings):
         """
         Config .env file for project credential and environment stuff
         """
-        env_file = '.env'
+        env_file = '.env'  # TODO why not working in app directory?
         env_file_encoding = 'utf-8'
         fields_list = ['SECRET_KEY', 'MONGO_HOST', 'MONGO_USERNAME', 'MONGO_PORT', 'MONGO_PASSWORD', 'MONGO_DB']
         fields = {item: {'env': item} for item in fields_list}
