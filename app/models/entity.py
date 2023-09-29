d5779809ed25fcf74d0be89383f0d55931699c6a
Mehrab <mehrabmh81@yahoo.com>
2023-09-29 23:50:44 +0330

    feat: add delete (soft and hard) functionality to entity + doc for other methods

--- app/models/entity.py
+++ app/models/entity.py
@@ -5,7 +5,7 @@ from bson import ObjectId
 from bson.errors import InvalidId
 from motor.motor_asyncio import AsyncIOMotorCollection, AsyncIOMotorDatabase
 from pydantic import BaseModel, Field
-from pymongo.results import InsertOneResult
+from pymongo.results import InsertOneResult, DeleteResult, UpdateResult
 
 from app.core.services.response_service import responseService
 from app.core.services.time_service import TimeService
@@ -15,9 +15,9 @@ from app.dependencies.database import get_main_db
 class Entity(BaseModel, ABC):
     """
         This is for models which are DB documents (same as SQL tables) -  all entities of our application
-        todo: add orm methods and stuff to it + getter setters
+        todo: add repository architecture + getter setters
     """
-    id: str | None = None
+    id: str | None = None  # equivalent of _id in DB
     active: bool = True
     created_at: str = Field(default=str(TimeService.get_now()))
 
@@ -36,34 +36,51 @@ class Entity(BaseModel, ABC):
         return db[cls.get_collection_name()]
 
     @classmethod
-    def create_object_id(cls, document_id: str):
+    def create_object_id(cls, document_id: str | None = None):
+        """ To avoid any problem when getting document_id from input - maybe invalid! """
         try:
-            return ObjectId(document_id)
+            return ObjectId(document_id) if document_id else False
         except InvalidId:
             return False  # TODO: take other actions?
 
     @classmethod
-    async def find_by_id(cls, document_id: str, allow_none: bool = True,
-                         not_found_error_message: str = 'invalid id provided.') -> Self | None:
+    async def find_by_id(cls, document_id: str, allow_none: bool = True, message: str = 'id not found.') -> Self | None:
+        """ find entity from db, when no doc found, raise 404 on case allow_none or return None """
         collection = await cls.get_collection()
         db_data = await collection.find_one({"_id": cls.create_object_id(document_id)})
 
         if (not db_data) and (not allow_none):
-            return responseService.error_404(not_found_error_message)
+            return responseService.error_404(message)
 
         return cls.model_validate({**db_data, 'id': str(db_data['_id'])}) if db_data else None
 
     @classmethod
     async def list_find_many(cls, condition: dict, max_length: int = 1000) -> list[Self]:
+        """ Simple find_many with converting it to list of entities, not the most optimized way but handy! """
         collection = await cls.get_collection()
         documents = await collection.find(condition).to_list(max_length)
         return list(map(lambda doc: cls._convert_document_to_object(doc), documents))
 
     async def insert(self, exclude: set | None = None) -> InsertOneResult:
+        """ Simple insert the instance with ability to exclude some field """
         exclude = exclude if exclude else set()
         exclude.add('id')
+
         collection = await self.get_collection()
         return await collection.insert_one(self.model_dump(exclude=exclude))
+
+    async def delete(self, soft: bool = False) -> bool:
+        """ Soft delete for now means deactivating the entity! just to make active = false in db """
+        collection = await self.get_collection()
+        if soft:
+            update_result: UpdateResult = await collection.update_one(
+                {'_id': ObjectId(self.id)},
+                {'$set': {'active': False}}
+            )
+            return update_result.acknowledged and bool(update_result.modified_count)
+
+        delete_result: DeleteResult = await collection.delete_one({'_id': ObjectId(self.id)})
+        return delete_result.acknowledged and bool(delete_result.deleted_count)
 
     @classmethod
     def _convert_document_to_object(cls, db_data: dict | None) -> Self:
