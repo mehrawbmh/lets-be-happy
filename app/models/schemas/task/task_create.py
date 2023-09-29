d7de6fbeb773c5ce08c6d88d235fcd626a40611b
Mehrab <mehrabmh81@yahoo.com>
2023-09-29 18:58:55 +0330

    better schema for task creation

--- app/models/schemas/task/task_create.py
+++ app/models/schemas/task/task_create.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from typing_extensions import Literal
 
 from app.core.enum.task_priorities import TaskPriority
+from app.core.enum.task_statuses import TaskStatus
 from app.core.services.time_service import TimeService
 from app.models.schema import Schema
 
@@ -10,7 +11,6 @@ from app.models.schema import Schema
 class CreateTask(Schema):
     assignee_username: str
     deadline: datetime = TimeService.get_today_end()
-    finished_at: datetime | None = None
+    status: Literal[TaskStatus.BACKLOG, TaskStatus.IN_PROGRESS] = TaskStatus.BACKLOG
     priority: TaskPriority = TaskPriority.MEDIUM
     description: str
-    saf: Literal['aa'] = 'aa'
