7c1ec9869a2da62cc407b39f9d5b168cc21d13e0
Mehrab <mehrabmh81@yahoo.com>
2023-10-04 21:00:40 +0330

    better phone validation (only for Iranians:) )

--- app/models/entities/users.py
+++ app/models/entities/users.py
@@ -13,7 +13,7 @@ class User(Entity):
     # id: str | None
     username: str
     password: str
-    phone: str = Field(min_length=11, max_length=11)
+    phone: str = Field()
     role: str = Role.USER
     email: str | None = Field(default=None)
 
@@ -72,6 +72,14 @@ class User(Entity):
             return responseService.error_400('wrong email format!')
 
         return email
+
+    @field_validator('phone')
+    @classmethod
+    def validate_phone_number(cls, phone: str, info: ValidationInfo):
+        if phone.startswith('09') and len(phone) == 11:
+            return phone
+
+        return responseService.error_400('bad phone nubmer given')
 
     @classmethod
     def check_raw_password(cls, password: str):
