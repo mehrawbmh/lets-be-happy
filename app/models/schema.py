6021d12d5933398d50c94000bd8b5891be94a6a0
Mehrab <mehrabmh81@yahoo.com>
2023-10-05 14:23:44 +0330

    make Schema abstract class

--- app/models/schema.py
+++ app/models/schema.py
@@ -1,7 +1,9 @@
+from abc import ABC
+
 from app.models.model import Model
 
 
-class Schema(Model):
+class Schema(Model, ABC):
     """
         This should be for models which aren't DB documents, but just schemas used on non-db layers.
         You have to specify use case when you want to add schema. e.g: there's just one user entity, but it can contain
