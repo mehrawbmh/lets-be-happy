589433734a53ac0f955d3df276638992125a4ffd
mehrawbmh <mehrabmh81@yahoo.com>
2023-10-12 00:30:09 +0330

    feat: add basic http auth to api docs + minor middleware fix

--- app/main.py
+++ app/main.py
@@ -1,12 +1,16 @@
-import time
+import time, secrets
+from typing import Annotated
 from fastapi import FastAPI, Depends, APIRouter
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
+from fastapi.openapi.docs import get_redoc_html, get_swagger_ui_html
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
 from motor.motor_asyncio import AsyncIOMotorDatabase
 
 from app.configs.settings import settings
 from app.core.database.indexes import DatabaseIndexManager
 from app.core.enum.event_types import EventTypes
+from app.core.services.response_service import responseService
 from app.dependencies.database import get_main_db
 from app.endpoints.tasks import router as tasks_router
 from app.endpoints.users import router as users_router
@@ -21,7 +25,6 @@ async def root():
 
 @router.get("/hello/{name}")
 async def health_test(name: str, message: str = '', db: AsyncIOMotorDatabase = Depends(get_main_db)):
-    print('new version')
     message = 'you said ' + message if message else ''
     resp = {
         'message': f"Hello {name}. It seems working fine ^_^. {message}"
@@ -37,7 +40,8 @@ app = FastAPI(
     summary="this is the back-end side of the open source project called let's be happy.",
     description="Further information will be published soon",
     version="1.0.0",
-    redoc_url="/docs2",
+    docs_url=None,
+    redoc_url=None,
 )
 
 app.include_router(router)
@@ -56,5 +60,27 @@ async def process_time_response(request, call_next):
     start = time.time()
     response = await call_next(request)
     end = time.time()
-    response.headers['X-processd-ms'] = 1000 * (end - start)  # in milli seconds
+    response.headers['X-processd-ms'] = str(1000 * (end - start))  # in milli seconds
     return response
+
+
+@app.get("/docs", include_in_schema=False)
+async def get_swagger_api_docs(credentials: Annotated[HTTPBasicCredentials, Depends(HTTPBasic())]):
+    if secrets.compare_digest(credentials.username, settings.DOC_USERNAME) and secrets.compare_digest(credentials.password, settings.DOC_PASSWORD):
+        return get_swagger_ui_html(
+            openapi_url=app.openapi_url,
+            title=app.title + " - Swagger UI",
+        )
+    
+    return responseService.error_403('wrong username or password given')
+
+
+@app.get("/redoc", include_in_schema=False)
+async def get_redoc_api_docs(credentials: Annotated[HTTPBasicCredentials, Depends(HTTPBasic())]):
+    if secrets.compare_digest(credentials.username, settings.DOC_USERNAME) and secrets.compare_digest(credentials.password, settings.DOC_PASSWORD):
+        return get_redoc_html(
+            openapi_url=app.openapi_url,
+            title=app.title + " - ReDoc",
+        )
+
+    return responseService.error_403('wrong username or password given')    
